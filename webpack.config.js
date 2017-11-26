4ef53a8e930899c8dc44a3e0e145ac0bc7d995e3
Kuba Turek <ja@kubaturek.pl>
2017-11-26 21:08:35 +0100

    Create sample input for webpack app

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname, 'src', 'test.js')
+    },
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].[chunkhash].js',
+        publicPath: '/',
+        chunkFilename: '[name].[chunkhash].js'
+    },
+    module:{
+        loaders: [
+            {
+                test: /\.js/, loader: 'babel-loader', exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        })
+    ]
+};
