b91dc65b52a85501121960356c426bf5ca13c0bc
Kuba Turek <ja@kubaturek.pl>
2017-12-02 22:15:56 +0100

    Let Searchbar fill whole length

--- src/components/header/SearchBar.js
+++ src/components/header/SearchBar.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Search from "grommet/components/Search";
-import Box from "grommet/components/Box";
 
 export default class SearchBar extends React.Component {
     render() {
         return (
-            <Box>
-                <Search placeHolder="Search" fill={true} inline={true}/>
-            </Box>
+            <Search placeHolder="Search" fill={true} inline={true}/>
         );
     }
 }
