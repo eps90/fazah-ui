aee5c27f2ef979b9867766f44887e71efc5041a3
Kuba Turek <ja@kubaturek.pl>
2017-12-02 16:06:24 +0100

    Insert Fazah logo to Logo component

--- src/components/sidebar/Logo.js
+++ src/components/sidebar/Logo.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import FazahLogo from './../../img/fazah-logo.svg';
+import Box from "grommet/components/Box";
 
 export default class Logo extends React.Component {
     render() {
         return (
-            <div>This is a logo</div>
+            <Box className="logo" pad='medium'>
+                <FazahLogo width='100%' height='auto' />
+            </Box>
         );
     }
 }
