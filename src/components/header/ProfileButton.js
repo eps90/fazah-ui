139933443df8099b73d49a70cce2be7699f18630
Kuba Turek <ja@kubaturek.pl>
2017-12-03 20:13:18 +0100

    Add mock of profile button

--- src/components/header/ProfileButton.js
+++ src/components/header/ProfileButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import UserIcon from 'grommet/components/icons/base/User'
+import Menu from "grommet/components/Menu";
+import Anchor from "grommet/components/Anchor";
 
 export default class ProfileButton extends React.Component {
     render() {
         return (
-            <div>This is a profile pic</div>
+            <Menu label='John Smith' icon={<UserIcon />} reverse={false}>
+                <Anchor>Settings</Anchor>
+                <Anchor>Logout</Anchor>
+            </Menu>
         );
     }
 }
