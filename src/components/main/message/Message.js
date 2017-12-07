3f47c865f4ab3a16551d87301d9de0cf01484895
Kuba Turek <ja@kubaturek.pl>
2017-12-07 21:00:20 +0100

    Create a wrapper for message

---
+++ src/components/main/message/Message.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Notification from "grommet/components/Notification";
+
+export default class Message extends React.Component {
+    render() {
+        return <Notification closer={false} size='medium' {...this.props} />
+    }
+}
