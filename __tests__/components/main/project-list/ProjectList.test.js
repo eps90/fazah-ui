fbffac4928d9d08cf7e33daa22ba92156318a3af
Kuba Turek <ja@kubaturek.pl>
2017-12-07 21:00:36 +0100

    Show a message when no projects are found

--- __tests__/components/main/project-list/ProjectList.test.js
+++ __tests__/components/main/project-list/ProjectList.test.js
@@ -3,6 +3,7 @@ import {shallow} from "enzyme";
 import ProjectList from "../../../../src/components/main/project-list/ProjectList";
 import ProjectListItem from "../../../../src/components/main/project-list/ProjectListItem";
 import Project from "../../../../src/model/Project";
+import Message from "../../../../src/components/main/message/Message";
 
 describe('ProjectList component', () => {
     test('should render a list of provided projects', () => {
@@ -13,4 +14,10 @@ describe('ProjectList component', () => {
         const wrapper = shallow(<ProjectList projects={projects} />);
         expect(wrapper.find(ProjectListItem)).toHaveLength(2);
     });
+
+    test('should show a message when no projects are found', () => {
+        const projects = [];
+        const wrapper = shallow(<ProjectList projects={projects}/>);
+        expect(wrapper.find(Message)).toHaveLength(1);
+    })
 });
--- src/components/main/project-list/ProjectList.js
+++ src/components/main/project-list/ProjectList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectListItem from "./ProjectListItem";
+import Message from "../message/Message";
 
 export default class ProjectList extends React.Component {
     constructor(props) {
@@ -7,8 +8,17 @@ export default class ProjectList extends React.Component {
     }
 
     render() {
-        return <ul>
-            {this.props.projects.map(el => <li key={el.id}><ProjectListItem project={el}/></li>)}
-        </ul>
+        let result;
+        if (!this.props.projects || this.props.projects.length === 0) {
+            result = <Message status='disabled' message='No projects to show' />;
+        } else {
+            result = <ul>
+                {this.props.projects.map((el) =>
+                    <li key={el.id}><ProjectListItem/></li>
+                )}
+            </ul>;
+        }
+
+        return result;
     }
 }
