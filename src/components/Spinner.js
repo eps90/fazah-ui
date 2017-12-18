981e57e63b6aab5a0dc1b1fcb9ea037722942f1e
Kuba Turek <ja@kubaturek.pl>
2017-12-18 20:12:50 +0100

    Create a spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Spinning from 'grommet/components/icons/Spinning';
+
+export default class Spinner extends React.Component {
+    render() {
+        return <Spinning size='large' />
+    }
+}
