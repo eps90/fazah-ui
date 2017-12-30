782b847972976e1e7b9bc0fafc1cd746b55e6966
Kuba Turek <ja@kubaturek.pl>
2017-12-30 18:39:36 +0100

    Remove getters from Metadata object

--- src/model/Metadata.js
+++ src/model/Metadata.js
@@ -1,26 +1,10 @@
 // @flow
 
 export default class Metadata {
-    _createdAt: Date;
-    _updatedAt: Date;
-    _enabled: boolean;
-
     constructor(createdAt: Date, updatedAt: ?Date, enabled: boolean) {
-        this._createdAt = createdAt;
-        this._updatedAt = updatedAt;
-        this._enabled = enabled;
-    }
-
-    get createdAt(): Date {
-        return this._createdAt;
-    }
-
-    get updatedAt(): Date {
-        return this._updatedAt;
-    }
-
-    get enabled(): boolean {
-        return this._enabled;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.enabled = enabled;
     }
 
     static empty() {
