de8f4ef507288d8cb530418b0bdf97277c76612e
Kuba Turek <ja@kubaturek.pl>
2017-12-30 22:29:36 +0100

    Use Redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './store/reducers';
@@ -12,7 +12,8 @@ import sagas from './store/sagas';
 import 'grommet/scss/vanilla/index.scss';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(sagas);
 
 ReactDOM.render(
