21df4a1cf35922117390e59faa8544c682ab97e9
Kuba Turek <ja@kubaturek.pl>
2017-12-30 12:54:19 +0100

    Initalize redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './containers/reducers';
 
 import 'grommet/scss/vanilla/index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
