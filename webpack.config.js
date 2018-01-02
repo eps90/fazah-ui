040a189217df2b5c1e4eaa677ac76c64f8be9fd8
Kuba Turek <ja@kubaturek.pl>
2018-01-02 17:24:20 +0100

    Load .env file during webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackCleanupPlugin = require("webpack-cleanup-plugin");
+const DotEnvPlugin = require("dotenv-webpack");
+
 module.exports = {
     entry: {
         app: path.resolve(__dirname, "src", "index.js")
@@ -42,6 +44,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new WebpackCleanupPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html"),
         }),
@@ -50,6 +53,6 @@ module.exports = {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV)
             }
         }),
-        new WebpackCleanupPlugin()
+        new DotEnvPlugin()
     ]
 };
