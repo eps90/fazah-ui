f5e3bbc9f3a61b484eb1e34ba93455306a80fb15
Kuba Turek <ja@kubaturek.pl>
2018-01-04 15:23:04 +0100

    Add a route for catalogues listing

--- src/components/layout/main/Main.js
+++ src/components/layout/main/Main.js
@@ -3,6 +3,7 @@ import Header from "../header/Header";
 import Box from "grommet/components/Box";
 import ProjectListContainer from "../../../containers/project/ProjectListContainer";
 import {Redirect, Route, Switch} from "react-router-dom";
+import CatalogueListRoute from "../../catalogue/catalogue-list/CatalogueListRoute";
 
 export default class Main extends React.Component {
     render() {
@@ -12,6 +13,7 @@ export default class Main extends React.Component {
                 <Box pad='medium'>
                     <Switch>
                         <Route path="/projects" exact component={ProjectListContainer} />
+                        <Route path="/projects/:projectId/catalogues" exact component={CatalogueListRoute} />
                         {/* TEMP */}
                         <Redirect to="/projects" from="/"/>
                     </Switch>
