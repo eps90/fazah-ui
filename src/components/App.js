1fca0622cf7dbe1c84479b06e7d78e4088983b43
Kuba Turek <ja@kubaturek.pl>
2018-01-06 17:03:38 +0100

    Change App template

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
 import React from "react";
-import GrommetApp from "grommet/components/App";
-import Split from "grommet/components/Split";
-import Box from "grommet/components/Box";
-import Sidebar from "./layout/sidebar/Sidebar";
 import Main from "./layout/main/Main";
+import {Container} from "semantic-ui-react";
+import Header from "./layout/header/Header";
 
 export default class App extends React.Component {
     render() {
         return (
-            <GrommetApp centered={false}>
-                <Box full={true}>
-                    <Split fixed={true} flex='right'>
-                        <Sidebar />
-                        <Main/>
-                    </Split>
-                </Box>
-            </GrommetApp>
+            <div>
+                <Header />
+                <Container>
+                    <Main/>
+                </Container>
+            </div>
         );
     }
 }
