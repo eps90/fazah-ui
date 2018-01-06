07fed6fe7cc10298a84229f9dd9918af0dd5ff57
Kuba Turek <ja@kubaturek.pl>
2018-01-06 17:03:28 +0100

    Reduce Main component to AppRoutes

--- src/components/layout/main/Main.js
+++ src/components/layout/main/Main.js
@@ -1,17 +1,10 @@
 import React from "react";
-import Header from "../header/Header";
-import Box from "grommet/components/Box";
 import AppRoutes from "../../AppRoutes";
 
 export default class Main extends React.Component {
     render() {
         return (
-            <Box>
-                <Header />
-                <Box pad='medium'>
-                    <AppRoutes />
-                </Box>
-            </Box>
+            <AppRoutes />
         );
     }
 }
