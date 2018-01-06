2b38c08c0132bea4a785d68079f99d0c5b58a067
Kuba Turek <ja@kubaturek.pl>
2018-01-06 19:19:47 +0100

    Provide a link to catalogues in project list item

--- src/components/project/project-list/ProjectListItem.js
+++ src/components/project/project-list/ProjectListItem.js
@@ -2,15 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import Project from "../../../model/Project";
 import {List} from "semantic-ui-react";
+import {Link} from "react-router-dom";
 
 export default class ProjectListItem extends React.Component {
     static propTypes = {
         project: PropTypes.instanceOf(Project).isRequired
     };
 
+    getCataloguesLink() {
+        return `/projects/${this.props.project.id}/catalogues`;
+    }
+
     render() {
         return (
-            <List.Header>{this.props.project.name}</List.Header>
+            <List.Header as={Link} to={this.getCataloguesLink()}>{this.props.project.name}</List.Header>
         );
     }
 }
