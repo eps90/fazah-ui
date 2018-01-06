34fdf06af9a2b0feb974a39490da1d0562354424
Kuba Turek <ja@kubaturek.pl>
2018-01-06 17:02:48 +0100

    How menu in a header

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,18 +1,15 @@
 import React from "react";
-import SearchBar from "./SearchBar";
-import ProfileButton from "./ProfileButton";
-import Box from "grommet/components/Box";
-import GrommetHeader from "grommet/components/Header";
+import {Menu} from "semantic-ui-react";
+import Logo from "../sidebar/Logo";
 
 export default class Header extends React.Component {
     render() {
         return (
-            <Box>
-                <GrommetHeader colorIndex='neutral-1-t' pad='medium'>
-                    <SearchBar/>
-                    <ProfileButton />
-                </GrommetHeader>
-            </Box>
+            <Menu fluid={true}>
+                <Menu.Item fitted='horizontally'>
+                    <Logo />
+                </Menu.Item>
+            </Menu>
         );
     }
 }
