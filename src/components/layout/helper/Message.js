d05ab0f9cc36dab82a89af07ead4789c4d463d82
Kuba Turek <ja@kubaturek.pl>
2018-01-06 18:59:33 +0100

    Remove unnecessary components

--- src/components/layout/helper/Message.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Notification from "grommet/components/Notification";
-
-export default class Message extends React.Component {
-    render() {
-        return <Notification closer={false} size='medium' {...this.props} />;
-    }
-}
--- src/components/layout/helper/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Spinning from "grommet/components/icons/Spinning";
-
-export default class Spinner extends React.Component {
-    render() {
-        return <Spinning size='large' />;
-    }
-}
