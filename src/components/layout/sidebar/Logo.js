04cd41d40735a8ea323b56c7a96742fb3e4375f7
Kuba Turek <ja@kubaturek.pl>
2018-01-06 17:02:16 +0100

    Adapt Logo component

--- src/components/layout/sidebar/Logo.js
+++ src/components/layout/sidebar/Logo.js
@@ -1,14 +1,11 @@
 import React from "react";
 import FazahLogo from "../../../img/fazah-logo.svg";
-import Box from "grommet/components/Box";
 import "./Logo.scss";
 
 export default class Logo extends React.Component {
     render() {
         return (
-            <Box>
-                <FazahLogo className='logo' />
-            </Box>
+            <FazahLogo className='logo' />
         );
     }
 }
