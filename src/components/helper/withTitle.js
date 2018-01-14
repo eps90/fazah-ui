1dcfeedfee4a0d70b4f06fc96f11c303e66c35e6
Kuba Turek <ja@kubaturek.pl>
2018-01-14 13:47:16 +0100

    Allow to pass subtitle to withTitle HOC

--- src/components/helper/withTitle.js
+++ src/components/helper/withTitle.js
@@ -1,15 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {Header} from "semantic-ui-react";
 
 export const withTitle = (titleContents) => {
     return (WrappedComponent) => {
         const hoc = (props) => {
+            const {subtitle, ...rest} = props;
+            const subtitleContents = subtitle
+                ? <Header.Subheader>{subtitle}</Header.Subheader>
+                : "";
+
             return (
                 <div>
-                    <Header as='h2'>{titleContents}</Header>
-                    <WrappedComponent {...props} />
+                    <Header as='h2'>
+                        {titleContents}
+                        {subtitleContents}
+                    </Header>
+                    <WrappedComponent {...rest} />
                 </div>
             );
+        };
+        hoc.propTypes = {
+            subtitle: PropTypes.string
         };
         hoc.displayName = `withTitle(${getDisplayName(WrappedComponent)})`;
         return hoc;
--- src/components/helper/withTitle.test.js
+++ src/components/helper/withTitle.test.js
@@ -12,6 +12,36 @@ describe("withTitle helper", () => {
         expect(wrapper.find(TestComponent)).toHaveLength(1);
         expect(wrapper.find(Header)).toHaveLength(1);
     });
+
+    describe("Sub header", () => {
+        it("should not render a sub-header when it's not provided in props", () => {
+            const titleContents = "This is a title";
+            const WrappedComponent = withTitle(titleContents)(TestComponent);
+            const wrapper = shallow(<WrappedComponent />);
+
+            expect(wrapper.find(Header.Subheader)).toHaveLength(0);
+        });
+
+        it("should display a subtitle when it is passed in props", () => {
+            const titleContents = "This is a title";
+            const subtitleContents = "This is a subtitle";
+
+            const WrappedComponent = withTitle(titleContents)(TestComponent);
+            const wrapper = shallow(<WrappedComponent subtitle={subtitleContents} />);
+
+            expect(wrapper.find(Header.Subheader)).toHaveLength(1);
+        });
+
+        it("should not pass subtitle prop to wrapped component", () => {
+            const titleContents = "This is a title";
+            const subtitleContents = "This is a subtitle";
+
+            const WrappedComponent = withTitle(titleContents)(TestComponent);
+            const wrapper = shallow(<WrappedComponent subtitle={subtitleContents} otherProp="blablabla" />);
+
+            expect(wrapper.find(TestComponent).first().props().subtitle).not.toBeDefined();
+        });
+    });
 });
 
 class TestComponent extends React.Component {
