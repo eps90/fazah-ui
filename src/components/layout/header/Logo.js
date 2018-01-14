e015a4624919aecbf289fe1b1e90498483d15563
Kuba Turek <ja@kubaturek.pl>
2018-01-14 12:35:01 +0100

    Allow to create inverted Logo

--- src/components/layout/header/Logo.js
+++ src/components/layout/header/Logo.js
@@ -1,11 +1,25 @@
 import React from "react";
 import FazahLogo from "../../../img/fazah-logo.svg";
+import PropTypes from "prop-types";
 import "./Logo.scss";
 
 export default class Logo extends React.Component {
+    static propTypes = {
+        inverted: PropTypes.bool
+    };
+
+    static defaultProps = {
+        inverted: false
+    };
+
     render() {
+        const classNames = ["logo"];
+        if (this.props.inverted) {
+            classNames.push("inverted");
+        }
+
         return (
-            <FazahLogo className='logo' />
+            <FazahLogo className={classNames.join(" ")} />
         );
     }
 }
