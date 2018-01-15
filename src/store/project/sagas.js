8a29719c7e2ed23e9bb59c51ecd0e2a63ba5b53f
Kuba Turek <ja@kubaturek.pl>
2018-01-15 21:51:50 +0100

    Fetch project from external resource

--- src/store/project/sagas.js
+++ src/store/project/sagas.js
@@ -1,6 +1,10 @@
-import {fetchProjects} from "../../service/repository/project";
-import {call, put, takeLatest} from "redux-saga/effects";
-import {failProjectListing, PROJECTS_REQUESTED, setProjects} from "./actions";
+import {fetchProject, fetchProjects} from "../../service/repository/project";
+import {call, put, select, takeLatest, all} from "redux-saga/effects";
+import {
+    failProjectListing, PROJECT_SELECTION_REQUESTED, PROJECTS_REQUESTED, selectProjectFailure, selectProjectSuccess,
+    setProjects
+} from "./actions";
+import {getProject} from "./../selectors";
 
 export function* fetchProjectList() {
     try {
@@ -11,6 +15,23 @@ export function* fetchProjectList() {
     }
 }
 
+export function* selectOrFetchProject({projectId}) {
+    try {
+        let project = yield select(getProject, projectId);
+        if (project === null) {
+            project = yield call(fetchProject, projectId);
+        }
+
+        yield put(selectProjectSuccess(project));
+    } catch (e) {
+        yield put(selectProjectFailure());
+    }
+
+}
+
 export default function* watchProjects() {
-    yield takeLatest(PROJECTS_REQUESTED, fetchProjectList);
+    yield all([
+        takeLatest(PROJECTS_REQUESTED, fetchProjectList),
+        takeLatest(PROJECT_SELECTION_REQUESTED, selectOrFetchProject)
+    ]);
 }
--- src/store/project/sagas.test.js
+++ src/store/project/sagas.test.js
@@ -1,58 +1,152 @@
 import {expectSaga, testSaga} from "redux-saga-test-plan";
 import * as matchers from "redux-saga-test-plan/matchers";
 import {throwError} from "redux-saga-test-plan/providers";
-import {failProjectListing, listProjects, setProjects} from "./actions";
-import watchProjects, {fetchProjectList} from "./sagas";
-import {fetchProjects} from "../../service/repository/project";
+import {
+    failProjectListing, listProjects, selectProject, selectProjectFailure, selectProjectSuccess,
+    setProjects
+} from "./actions";
+import watchProjects, {fetchProjectList, selectOrFetchProject} from "./sagas";
+import {fetchProject, fetchProjects} from "../../service/repository/project";
+import {getProject} from "./../selectors";
 
 describe("projects sagas", () => {
-    describe("unit", () => {
-        it("should dispatch action with fetched projects", () => {
-            const projectsFetched = [{id: 1}, {id: 2}];
-            testSaga(fetchProjectList)
-                .next()
-                .call(fetchProjects)
-                .next(projectsFetched)
-                .put(setProjects(projectsFetched))
-                .next()
-                .isDone();
+    describe("Fetch all projects", () => {
+        describe("unit", () => {
+            it("should dispatch action with fetched projects", () => {
+                const projectsFetched = [{id: 1}, {id: 2}];
+                testSaga(fetchProjectList)
+                    .next()
+                    .call(fetchProjects)
+                    .next(projectsFetched)
+                    .put(setProjects(projectsFetched))
+                    .next()
+                    .isDone();
+            });
+
+            it("should dispatch error action when fetching projects fails", () => {
+                testSaga(fetchProjectList)
+                    .next()
+                    .call(fetchProjects)
+                    .throw()
+                    .put(failProjectListing())
+                    .next()
+                    .isDone();
+            });
         });
 
-        it("should dispatch error action when fetching projects fails", () => {
-            testSaga(fetchProjectList)
-                .next()
-                .call(fetchProjects)
-                .throw()
-                .put(failProjectListing())
-                .next()
-                .isDone();
+        describe("integration", () => {
+            it("should dispatch fetched projects", () => {
+                const projectsFetched = [
+                    {id: 1, name: "My first project"},
+                    {id: 2, name: "My second project"}
+                ];
+
+                return expectSaga(watchProjects)
+                    .provide([
+                        [matchers.call.fn(fetchProjects), projectsFetched]
+                    ])
+                    .put(setProjects(projectsFetched))
+                    .dispatch(listProjects())
+                    .silentRun();
+            });
+
+            it("should dispatch error when fetching projects fails", () => {
+                return expectSaga(watchProjects)
+                    .provide([
+                        [matchers.call.fn(fetchProjects), throwError("Fetching projects failed!")]
+                    ])
+                    .put(failProjectListing())
+                    .dispatch(listProjects())
+                    .silentRun();
+            });
         });
     });
 
-    describe("integration", () => {
-        it("should dispatch fetched projects", () => {
-            const projectsFetched = [
-                {id: 1, name: "My first project"},
-                {id: 2, name: "My second project"}
-            ];
+    describe("Select project", () => {
+        describe("unit", () => {
+            it("should select a project from available projects", () => {
+                const projectId = "1111";
+                const foundProject = {"id": "1111", "name": "My awesome project"};
 
-            return expectSaga(watchProjects)
-                .provide([
-                    [matchers.call.fn(fetchProjects), projectsFetched]
-                ])
-                .put(setProjects(projectsFetched))
-                .dispatch(listProjects())
-                .silentRun();
+                testSaga(selectOrFetchProject, {projectId})
+                    .next()
+                    .select(getProject, projectId)
+                    .next(foundProject)
+                    .put(selectProjectSuccess(foundProject))
+                    .next()
+                    .isDone();
+            });
+
+            it("should fetch a project when it is not found in the store", () => {
+                const projectId = "1111";
+                const fetchedProject = {"id": "1111", "name": "My awesome project"};
+
+                testSaga(selectOrFetchProject, {projectId})
+                    .next()
+                    .select(getProject, projectId)
+                    .next(null)
+                    .call(fetchProject, projectId)
+                    .next(fetchedProject)
+                    .put(selectProjectSuccess(fetchedProject))
+                    .next()
+                    .isDone();
+            });
+
+            it("should send an error when fetching a project fails", () => {
+                const projectId = "1111";
+
+                testSaga(selectOrFetchProject, {projectId})
+                    .next()
+                    .select(getProject, projectId)
+                    .next(null)
+                    .call(fetchProject, projectId)
+                    .throw()
+                    .put(selectProjectFailure())
+                    .next()
+                    .isDone();
+            });
         });
 
-        it("should dispatch error when fetching projects fails", () => {
-            return expectSaga(watchProjects)
-                .provide([
-                    [matchers.call.fn(fetchProjects), throwError("Fetching projects failed!")]
-                ])
-                .put(failProjectListing())
-                .dispatch(listProjects())
-                .silentRun();
+        describe("integration", () => {
+            it("should select a project from available projects", () => {
+                const projectId = "1111";
+                const foundProject = {"id": "1111", "name": "My awesome project"};
+
+                return expectSaga(watchProjects)
+                    .provide([
+                        [matchers.select(getProject, projectId), foundProject]
+                    ])
+                    .put(selectProjectSuccess(foundProject))
+                    .dispatch(selectProject(projectId))
+                    .silentRun();
+            });
+
+            it("should fetch a project when it is not found in the store", () => {
+                const projectId = "1111";
+                const fetchedProject = {"id": "1111", "name": "My awesome project"};
+
+                return expectSaga(watchProjects)
+                    .provide([
+                        [matchers.select(getProject, projectId), null],
+                        [matchers.call.fn(fetchProject, projectId), fetchedProject]
+                    ])
+                    .put(selectProjectSuccess(fetchedProject))
+                    .dispatch(selectProject(projectId))
+                    .silentRun();
+            });
+
+            it("should send an error when fetching a project fails", () => {
+                const projectId = "1111";
+
+                return expectSaga(watchProjects)
+                    .provide([
+                        [matchers.select(getProject, projectId), null],
+                        [matchers.call.fn(fetchProject, projectId), throwError("Fetching project failed!")]
+                    ])
+                    .put(selectProjectFailure())
+                    .dispatch(selectProject(projectId))
+                    .silentRun();
+            });
         });
     });
 });
