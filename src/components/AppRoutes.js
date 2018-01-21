80dca637d651a6e509d1a007cabf4c4cdea09504
Kuba Turek <ja@kubaturek.pl>
2018-01-21 20:38:26 +0100

    [#3] Migrate current routes to route config

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,16 +1,7 @@
 import React from "react";
-import {Redirect, Route, Switch} from "react-router-dom";
-import ProjectListContainer from "../containers/project/ProjectListContainer";
-import CatalogueListRoute from "./catalogue/catalogue-list/CatalogueListRoute";
-import {fullContainer, smallContainer} from "./helper/layout";
+import RoutesFromConfig from "../routing/components/RoutesFromConfig";
+import getContentRoutes from "../routing/helpers/getContentRoutes";
 
-const AppRoutes = () => (
-    <Switch>
-        <Route path="/projects" exact component={smallContainer(ProjectListContainer)} />
-        <Route path="/projects/:projectId/catalogues" exact component={fullContainer(CatalogueListRoute)} />
-        {/* TEMP */}
-        <Redirect to="/projects" from="/"/>
-    </Switch>
-);
+const AppRoutes = () => <RoutesFromConfig config={getContentRoutes()} />;
 
 export default AppRoutes;
