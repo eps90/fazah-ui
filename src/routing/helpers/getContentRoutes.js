9b6d27a6d8493b67fc99f594f39b488cb6899d7f
Kuba Turek <ja@kubaturek.pl>
2018-01-21 20:29:25 +0100

    [#3] Return redirects from getContentRoutes

--- src/routing/helpers/getContentRoutes.js
+++ src/routing/helpers/getContentRoutes.js
@@ -4,7 +4,7 @@ export default function getContentRoutes() {
     const result = [];
     for (const routeName in routingConfig) {
         const route = routingConfig[routeName];
-        if ("component" in route) {
+        if ("component" in route || "redirectTo" in route) {
             result.push(route);
         }
     }
