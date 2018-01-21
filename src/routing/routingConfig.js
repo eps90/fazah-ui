b5ad1743b0e6b79c3ad4c5e72f125f47f569b465
Kuba Turek <ja@kubaturek.pl>
2018-01-21 20:38:26 +0100

    [#3] Migrate current routes to route config

--- src/routing/routingConfig.js
+++ src/routing/routingConfig.js
@@ -1,2 +1,22 @@
-const routingConfig = {};
+import ProjectListContainer from "../containers/project/ProjectListContainer";
+import {fullContainer, smallContainer} from "../components/helper/layout";
+import CatalogueListRoute from "../components/catalogue/catalogue-list/CatalogueListRoute";
+
+const routingConfig = {
+    projects: {
+        path: "/projects",
+        exact: true,
+        component: smallContainer(ProjectListContainer)
+    },
+    catalogues: {
+        path: "/projects/:projectId/catalogues",
+        exact: true,
+        component: fullContainer(CatalogueListRoute)
+    },
+    homepage: {
+        path: "/",
+        exact: true,
+        redirectTo: "/projects"
+    }
+};
 export default routingConfig;
