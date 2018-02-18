957a7c634e43513e6785ae2830e36c0fd4b2f8e1
Kuba Turek <ja@kubaturek.pl>
2018-02-18 13:32:24 +0100

    Show a toolbar when rendering CatalogueListRoute

--- src/components/catalogue/catalogue-list/CatalogueListRoute.js
+++ src/components/catalogue/catalogue-list/CatalogueListRoute.js
@@ -1,9 +1,14 @@
 import React from "react";
 import CatalogueListContainer from "../../../containers/catalogue/CatalogueListContainer";
 import PropTypes from "prop-types";
+import CatalogueToolbarContainer from "../../../containers/catalogue/CatalogueToolbarContainer";
 
 const CatalogueListRoute = ({match}) => {
-    return <CatalogueListContainer projectId={match.params.projectId} />;
+    const projectId = match.params.projectId;
+    const toolbar = <CatalogueToolbarContainer projectId={projectId}/>;
+    return (
+        <CatalogueListContainer projectId={projectId} toolbar={toolbar} />
+    );
 };
 
 CatalogueListRoute.propTypes = {
